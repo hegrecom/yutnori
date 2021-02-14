b0025c9576d785fec6e3097e7a431494f991cfdc
Taehwa Kang <hegrecomm@gmail.com>
2021-02-14 21:39:19 +0900

    feat: Return throw result as 도개걸윷모

--- src/libs/models/yut_set.rs
+++ src/libs/models/yut_set.rs
@@ -1,28 +1,44 @@
 use super::yut::Yut;
+use std::collections::HashMap;
 
-pub struct YutSet {
+pub struct YutSet<'a> {
     back_do: Yut,
-    normal_yut_set: Vec<Yut>
+    normal_yut_set: Vec<Yut>,
+    rank: HashMap<(usize, bool), &'a str>
 }
 
-impl YutSet {
+impl<'a> YutSet<'a> {
     pub fn new() -> Self {
         let back_do = Yut::new();
         let normal_yut_set = (0..3).map(|_| Yut::new()).collect();
+        let rank = [
+            ((1, false), "도"),
+            ((1, true), "빽도"),
+            ((2, false), "개"),
+            ((2, true), "개"),
+            ((3, false), "걸"),
+            ((3, true), "걸"),
+            ((4, false), "윷"),
+            ((4, true), "윷"),
+            ((0, false), "모"),
+            ((0, true), "모"),
+        ].iter().cloned().collect();
+
 
         YutSet {
             back_do,
-            normal_yut_set
+            normal_yut_set,
+            rank
         }
     }
 
-    pub fn throw(&self) -> (usize, bool) {
+    pub fn throw(&self) -> &str {
         let throwed_backdo: bool = self.back_do.throw();
         let mut throwed_result: Vec<bool> = self.normal_yut_set.iter().map(|yut| yut.throw()).collect();
 
         throwed_result.push(throwed_backdo);
         let flipped_count = throwed_result.iter().filter(|&result| *result == true).count();
 
-        (flipped_count, throwed_backdo)
+        self.rank.get(&(flipped_count, throwed_backdo)).unwrap()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 mod libs;
+use std::collections::HashMap;
 
 fn main() {
     let yut_set = libs::models::yut_set::YutSet::new();
+    let mut result: HashMap<&str, usize> = HashMap::new();
 
-    println!("{:?}", yut_set.throw());
+    (0..=1000000).for_each(|_| {
+        let throw = yut_set.throw();
+        let counter = result.entry(throw).or_insert(0);
+        *counter += 1;
+    });
+
+    println!("{:?}", result);
 }
